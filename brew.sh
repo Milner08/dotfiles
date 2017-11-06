47f44b3ab81fd9b7e7beaa929d0d5153227112de
Tomas Milner <tomas@betelgeuse.home>
2017-11-06 01:54:13 +0000

    ZSH over Bash.

--- brew.sh
+++ brew.sh
@@ -23,11 +23,12 @@ brew install gnu-sed --with-default-names
 # running `chsh`.
 brew install bash
 brew install bash-completion2
+brew install zsh
 
 # Switch to using brew-installed bash as default shell
-if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
-  echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
-  chsh -s /usr/local/bin/bash;
+if ! fgrep -q '/usr/local/bin/zsh' /etc/shells; then
+  echo '/usr/local/bin/zsh' | sudo tee -a /etc/shells;
+  chsh -s /usr/local/bin/zsh;
 fi;
 
 # Install `wget` with IRI support.
